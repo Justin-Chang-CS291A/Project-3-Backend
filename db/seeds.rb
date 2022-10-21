5778a187949bc234929efed4f88091f699fd4ab2
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-10-21 02:39:10 -0700

    Add seed data (#3)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(email: 'test1@email.com')
+User.create(email: 'test2@email.com')
+
+Post.create(content: 'Post 1 content by user 1', user_id: 1)
+Post.create(content: 'Post 2 content by user 1', user_id: 1)
+Post.create(content: 'Post 3 content by user 2', user_id: 2)
+
+Comment.create(content: 'Comment 1 content by user 1 on post 1', user_id: 1, post_id: 1)
+Comment.create(content: 'Comment 2 content by user 2 on post 1', user_id: 2, post_id: 1)
+Comment.create(content: 'Comment 3 content by user 1 on post 3', user_id: 1, post_id: 3)
