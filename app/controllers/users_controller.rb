beb3bfb6ccde3a731e98208e7043ea79f457b00b
Justin Chang <37165464+just-in-chang@users.noreply.github.com>
2022-10-21 21:38:58 -0700

    Enable user params (#7)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,8 +48,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
-    params.fetch(:user, {})
+    params.require(:user).permit(:email)
   end
 end
